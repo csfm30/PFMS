dc8a23cca2efcbc3d9cfd48bcbe84b3e9851fd06
csfm30 <csfmchakree@hotmail.com>
2024-09-25 11:47:52 +0700

    fix: add month and year input

--- api/report/expense_report.go
+++ api/report/expense_report.go
@@ -10,6 +10,11 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
+type reqExpenseReport struct {
+	Month string `json:"month"`
+	Year  string `json:"year"`
+}
+
 type resExpenseReport struct {
 	CategoryName string  `json:"category_name"`
 	TotalAmount  float64 `json:"total_amount"`
@@ -22,12 +27,20 @@ func ExpenseReport(c *fiber.Ctx) error {
 	userId := claims["user_id"].(string)
 	_ = userId
 
+	reqExpenseReport := new(reqExpenseReport)
 	resExpenseReport := []resExpenseReport{}
+
+	if err := c.BodyParser(reqExpenseReport); err != nil {
+		logs.Error(err)
+		return utility.ResponseError(c, fiber.StatusBadRequest, err.Error())
+	}
 
 	if err := db.Model(&modelsPg.Transaction{}).
 		Select("expense_categories.name AS category_name, SUM(transactions.amount) AS total_amount").
 		Joins("left join expense_categories ON expense_categories.id = transactions.expense_category_id").
 		Where("transactions.type = ? and transactions.user_id = ?", "expense", userId).
+		Where("EXTRACT(MONTH FROM transactions.date) = ?", reqExpenseReport.Month).
+		Where("EXTRACT(YEAR FROM transactions.date) = ?", reqExpenseReport.Year).
 		Group("expense_categories.name").
 		Find(&resExpenseReport).Error; err != nil {
 		logs.Error(err)
